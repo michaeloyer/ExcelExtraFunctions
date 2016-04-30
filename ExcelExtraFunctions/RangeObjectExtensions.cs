5703da6b778f1e1bb61b743ba250acdd59a7f1ec
michaeloyer <michael.oyer88@outlook.com>
2016-04-30 17:04:21 -0400

    Added Range to IEnumerable Extension Functions

--- ExcelExtraFunctions/RangeObjectExtensions.cs
+++ ExcelExtraFunctions/RangeObjectExtensions.cs
@@ -17,5 +17,32 @@ namespace ExcelExtraFunctions
         {
             return range.GetLength(1);
         }
+
+        /// <summary>
+        /// Converts a range to an IEnumerable by row and then by column.
+        /// </summary>
+        /// <param name="range">Intended for parameters that take in a
+        /// range and it is converted to a two dimensional object array by ExcelDNA</param>
+        /// <returns></returns>
+        internal static IEnumerable<object> SelectByRow(this object[,] range)
+        {
+            for (int x = 0; x < range.RowCount(); x++)
+                for (int y = 0; y < range.ColumnCount(); y++)
+                    yield return range[x, y];
+        }
+
+        /// <summary>
+        /// Converts a range to an IEnumerable by column and then by row.
+        /// </summary>
+        /// <param name="range">Intended for parameters that take in a
+        /// range and it is converted to a two dimensional object array by ExcelDNA</param>
+        /// <returns></returns>
+        internal static IEnumerable<object> SelectByColumn(this object[,] range)
+        {
+            for (int y = 0; y < range.ColumnCount(); y++)
+                for (int x = 0; x < range.RowCount(); x++)
+                    yield return range[x, y];
+        }
+
     }
 }
