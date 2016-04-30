173317122947634899055d77e08792dc42e30135
michaeloyer <michael.oyer88@outlook.com>
2016-04-30 10:06:51 -0400

    Added new Regular Expression functions
    IsMatch
    Escape
    Replace
    Count

---
+++ ExcelExtraFunctions/RegularExpressions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ExcelExtraFunctions
+{
+    public static class RegularExpressions
+    {
+        public static bool IsMatch(string input, string pattern) => Regex.IsMatch(input, pattern);
+        public static string Escape(string input) => Regex.Escape(input);
+        public static string Replace(string input, string pattern, string replacement) => Regex.Replace(input, pattern, replacement);
+        public static int Count(string input, string pattern) => Regex.Matches(input, pattern).Count;
+    }
+}
