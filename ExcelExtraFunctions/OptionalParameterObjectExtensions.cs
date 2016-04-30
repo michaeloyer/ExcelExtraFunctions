5f664fb16332fb7713f5875e19b819df876e363b
michaeloyer <michael.oyer88@outlook.com>
2016-04-30 16:53:46 -0400

    Added IfMissing Extension Functions

--- ExcelExtraFunctions/OptionalParameterObjectExtensions.cs
+++ ExcelExtraFunctions/OptionalParameterObjectExtensions.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExcelDna.Integration;
 
 namespace ExcelExtraFunctions
 {
     internal static class OptionalParameterObjectExtensions
     {
-        
+        internal static T IfMissing<T>(object parameter, T valueIfMissing) where T : struct
+        {
+            if (parameter is ExcelMissing)
+                return valueIfMissing;
+            else
+                return (T)parameter;
+        }
+
+        internal static string IfMissing<T>(object parameter, string valueIfMissing)
+        {
+            if (parameter is ExcelMissing)
+                return valueIfMissing;
+            else
+                return (string)parameter;
+        }
     }
 }
