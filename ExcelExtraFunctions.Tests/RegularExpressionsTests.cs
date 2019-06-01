90f68c965d0811c40f40de538cd29b6fc96afbc1
michaeloyer <code@michaeloyer.com>
2019-05-31 21:32:14 -0400

    Add ExtraExcelFunctions.Tests project

---
+++ ExcelExtraFunctions.Tests/RegularExpressionsTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static ExcelExtraFunctions.Functions.RegularExpressions;
+
+namespace ExcelExtraFunctions.Tests
+{
+    [TestClass]
+    public class RegularExpressionsTests
+    {
+        [DataTestMethod]
+        [DataRow("abc123", "[a-c]{3}[1-3]{3}", true)]
+        [DataRow("abc123", "[1-3]{3}[a-c]{3}", false)]
+        public void IsMatch_Tests(string source, string pattern, bool result)
+        {
+            IsMatch(source, pattern).Should().Equals(result);
+        }
+    }
+}
